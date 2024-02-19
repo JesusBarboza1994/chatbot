8a477c12569371aa10226cf5d537843eb89b2804
Jesus Barboza <jbarz20@gmail.com>
2024-02-19 17:24:55 -0500

    ADD: rnviar texto a numero externo

--- src/components/whatsapp/utils/sendResponseToWhatsapp.js
+++ src/components/whatsapp/utils/sendResponseToWhatsapp.js
@@ -3,18 +3,18 @@ import axios from "axios";
 export async function sendResponseToWhatsapp(body, response_chat, from_number=null){
   const token = process.env.WHATSAPP_TOKEN;
   try {
-    if(body.object){
-      if (
-        body.entry &&
-        body.entry[0].changes &&
-        body.entry[0].changes[0] &&
-        body.entry[0].changes[0].value.messages &&
-        body.entry[0].changes[0].value.messages[0]
-      ) {
+    // if(body.object){
+    //   if (
+    //     body.entry &&
+    //     body.entry[0].changes &&
+    //     body.entry[0].changes[0] &&
+    //     body.entry[0].changes[0].value.messages &&
+    //     body.entry[0].changes[0].value.messages[0]
+    //   ) {
         let phone_number_id = body.entry[0].changes[0].value.metadata.phone_number_id;
         let from = body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
         // let msg_body = body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-        await axios({
+        const response = await axios({
           method: "POST", // Required, HTTP method, a string, e.g. POST, GET
           url:
             "https://graph.facebook.com/v12.0/" +
@@ -28,8 +28,9 @@ export async function sendResponseToWhatsapp(body, response_chat, from_number=nu
           },
           headers: { "Content-Type": "application/json" },
         });
-      }
-    }
+        if(!from_number) console.log("RESPONSE WSP SEND", response.data)
+    //   }
+    // }
   } catch (error) {
     console.log("ERROR", error)
     throw error
