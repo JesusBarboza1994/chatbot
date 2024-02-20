21a6c2e3a5170c8ee66f9d075bd8fe1ac141d97b
Jesus Barboza <jbarz20@gmail.com>
2024-02-20 12:57:13 -0500

    ADD: logs of phone

--- src/components/whatsapp/utils/sendResponseToWhatsapp.js
+++ src/components/whatsapp/utils/sendResponseToWhatsapp.js
@@ -12,12 +12,14 @@ export async function sendResponseToWhatsapp(body, response_chat, from_number=nu
     //     body.entry[0].changes[0].value.messages[0]
     //   ) {
         let phone_number_id = body.entry[0].changes[0].value.metadata.phone_number_id;
+        console.log("🚀 ~ sendResponseToWhatsapp ~ phone_number_id:", phone_number_id)
         let from = body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+        console.log("🚀 ~ sendResponseToWhatsapp ~ from:", from)
         // let msg_body = body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
         const response = await axios({
           method: "POST", // Required, HTTP method, a string, e.g. POST, GET
           url:
-            "https://graph.facebook.com/v12.0/" +
+            "https://graph.facebook.com/v18.0/" +
             phone_number_id +
             "/messages?access_token=" +
             token,
